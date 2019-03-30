493f13c5c2f94e0a51ef463cdd72cd840cdf0fa2
jeffrey.miller445 <jeffrey.miller445@gmail.com>
2019-03-30 13:46:58 -0400

    navbar-framwork

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Main from "./components/pages/Main";
+import Maintenance from "./components/pages/Maintenance";
+import Rent from "./components/pages/Rent";
+import Return from "./components/pages/Return";
+import Inventory from "./components/pages/Inventory";
+
+// import About from "./components/pages/About";
+// import Blog from "./components/pages/Blog";
+// import Contact from "./components/pages/Contact";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <NavTabs />
+          <div className="App">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h2>Welcome to Outdoor Gear Bank</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Route exact path="/" component={Main} />
+          <Route exact path="/rent" component={Rent} />
+          <Route exact path="/return" component={Return} />
+          <Route exact path="/maintenance" component={Maintenance} />
+          <Route exact path="/inventory" component={Inventory} />
+
+          {/* <Route exact path="/calander" component={Calendar} /> */}
+          {/* <Route exact path="/about" component={About} />
+          <Route exact path="/blog" component={Blog} />
+          <Route path="/contact" component={Contact} /> */}
+        
+        <p className="App-main mt-3 ml-3">
+          Supplemental information display area. <code>...</code>
         </p>
       </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/pages/Maintenance.js
@@ -0,0 +1,52 @@
+// import React from "react";
+import React, { Component } from "react";
+import items from "../../available.json";
+
+class Maintenance extends Component {
+// function Available() {
+    state = {
+        items
+    };
+    
+  render() {
+  return (
+    <div className="container bg-light border">
+      <div className="col-1-md"></div>
+      <div className="col-10-md"></div>
+
+      <h1>Maintenance Page</h1>
+    
+      {this.state.items.map((item,index) => (
+    //    console.log("item.fname= ",item.fname)
+    <p>
+      {item.fname}
+    </p>
+      ))}
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
+        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
+        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
+        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
+        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
+        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
+        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
+        malesuada fames ac ante ipsum primis in faucibus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
+        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
+        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
+        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
+        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
+        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
+        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
+        malesuada fames ac ante ipsum primis in faucibus.
+      </p>
+      <div className="col-1-md"></div>
+    </div>
+  );
+    }
+}
+
+export default Maintenance;
