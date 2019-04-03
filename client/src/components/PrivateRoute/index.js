630512ed32855f01c1ea735294434ac0516f42b4
giantrotta24 <giantrotta@me.com>
2019-04-02 20:08:48 -0400

    omg i got it to work...

--- client/src/components/PrivateRoute/index.js
+++ client/src/components/PrivateRoute/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import {  Route, Redirect } from 'react-router-dom';
-import axios from 'axios';
-
 
 const PrivateRoute = ({ component: Component, ...rest }, isAuthenticated) => {
     
