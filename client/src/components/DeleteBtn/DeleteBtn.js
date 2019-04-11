cf5d0f94162f896e7c3a22f2e9e8fa2fd8f3bf2a
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-04-11 15:09:32 -0400

    cleaned up DeleteBtn and Footer components

--- client/src/components/DeleteBtn/DeleteBtn.js
+++ client/src/components/DeleteBtn/DeleteBtn.js
@@ -1,11 +1,9 @@
-import React from "react";
-import "./DeleteBtn.css";
+import React from 'react';
+import './DeleteBtn.css';
 
-// The ...props means, spread all of the passed props onto this element
-// That way we don't have to define them all individually
 function DeleteBtn(props) {
   return (
-    <span className="delete-btn btn btn-danger" {...props} role="button" tabIndex="0">
+    <span className='delete-btn btn btn-danger' {...props} role='button' tabIndex='0'>
       Put Item in Maintenance
     </span>
   );
--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -1,1 +1,1 @@
-export { default } from "./DeleteBtn";
+export { default } from './DeleteBtn';
