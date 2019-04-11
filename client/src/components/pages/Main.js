f01d3c6529a2b989d81431579c9db9bbd03de73e
giantrotta24 <giantrotta@me.com>
2019-04-10 20:27:55 -0400

    working modal

--- client/src/components/pages/Main.js
+++ client/src/components/pages/Main.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
+import Modal from '../Modal';
 import API from '../../utils/API';
+
+// const modalContent = (
+//   <div>
+//     <p>Hello world Lorem ipsum dolor sit amet, <a href="#1">first link</a> consectetur adipiscing elit. Phasellus sagittis erat ut ex bibendum consequat. Morbi luctus ex ex, at varius purus <a href="#2">second link</a> vehicula consectetur. Curabitur a sapien a augue consequat rhoncus. Suspendisse commodo ullamcorper nibh quis blandit. Etiam viverra neque quis mauris efficitur, lobortis aliquam ex pharetra. Nam et ante ex. Sed gravida gravida ligula, non blandit nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer consectetur efficitur tempor. Nunc sollicitudin felis congue facilisis faucibus. Mauris faucibus sit amet ante eleifend dapibus.</p>
+//   </div>
+// );
 
 
 class Main extends Component {
@@ -24,10 +31,18 @@ class Main extends Component {
     });
   }
 
+
+  modalContent = (
+    <div>
+      <p>Hello world Lorem ipsum dolor sit amet, <a href="#1">first link</a> consectetur adipiscing elit. Phasellus sagittis erat ut ex bibendum consequat. Morbi luctus ex ex, at varius purus <a href="#2">second link</a> vehicula consectetur. Curabitur a sapien a augue consequat rhoncus. Suspendisse commodo ullamcorper nibh quis blandit. Etiam viverra neque quis mauris efficitur, lobortis aliquam ex pharetra. Nam et ante ex. Sed gravida gravida ligula, non blandit nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer consectetur efficitur tempor. Nunc sollicitudin felis congue facilisis faucibus. Mauris faucibus sit amet ante eleifend dapibus.</p>
+    </div>
+  );
+
+
   render() {
     return (
-      <div className="container border">
-        <div className="row"></div>
+      <div>
+        <Modal>{this.modalContent}</Modal>
       </div>
     );
   }
