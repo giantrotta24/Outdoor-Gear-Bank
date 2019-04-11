3eb50b77d9fad4dd8e0001b729b55a2721425f78
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-04-11 15:49:52 -0400

    cleanup to Return page

--- client/src/components/pages/Return.js
+++ client/src/components/pages/Return.js
@@ -1,27 +1,27 @@
-// import React from "react";
-import React, { Component } from "react";
+// import React from 'react';
+import React, { Component } from 'react';
 import ReturnForm from '../ReturnForm';
 import ReturnResults from '../ReturnResults';
 import ReturnResultsItem from '../ReturnResultsItem';
 import API from '../../utils/API';
-import { Container, Row, Col } from "../Grid";
+import { Container, Row, Col } from '../Grid';
 import DeleteBtn from '../DeleteBtn';
 import ReturnBtn from '../ReturnBtn';
-import { SelectCondition, TextArea } from "../Form";
+import { SelectCondition, TextArea } from '../Form';
 
 class Return extends Component {
 
   state = {
-    last_name: "",
-    phone_number: "",
-    member_number: "",
-    email: "",
-    customerID: "",
+    last_name: '',
+    phone_number: '',
+    member_number: '',
+    email: '',
+    customerID: '',
     customers: [],
     results: [],
     items: [],
-    error: "",
-    itemID: "",
+    error: '',
+    itemID: '',
     itemCondition: [],
     itemComment: []
   };
@@ -44,64 +44,64 @@ class Return extends Component {
     if (this.state.last_name) {
       API.findCustomerByLastName(this.state.last_name)
         .then(res => {
-          if (res.data.status === "error") {
+          if (res.data.status === 'error') {
             throw new Error(res.data.message);
           }
           this.setState({
             results: res.data,
-            error: "",
-            customers: "",
+            error: '',
+            customers: '',
             items: res.data[0].items,
             customerID: res.data[0]._id,
-            customer: res.data[0].first_name + " " + res.data[0].last_name
+            customer: res.data[0].first_name + ' ' + res.data[0].last_name
           });
         })
         .catch(err => this.setState({ error: err.message }));
     } else if (this.state.phone_number) {
       API.findCustomerByPhoneNumber(this.state.phone_number)
         .then(res => {
-          if (res.data.status === "error") {
+          if (res.data.status === 'error') {
             throw new Error(res.data.message);
           }
           this.setState({
             results: res.data,
-            error: "",
-            customers: "",
+            error: '',
+            customers: '',
             items: res.data[0].items,
             customerID: res.data[0]._id,
-            customer: res.data[0].first_name + " " + res.data[0].last_name
+            customer: res.data[0].first_name + ' ' + res.data[0].last_name
           });
         })
         .catch(err => this.setState({ error: err.message }));
     } else if (this.state.member_number) {
       API.findCustomerByMemberNumber(this.state.member_number)
       .then(res => {
-        if (res.data.status === "error") {
+        if (res.data.status === 'error') {
           throw new Error(res.data.message);
         }
         this.setState({
           results: res.data,
-          error: "",
-          customers: "",
+          error: '',
+          customers: '',
           items: res.data[0].items,
           customerID: res.data[0]._id,
-          customer: res.data[0].first_name + " " + res.data[0].last_name
+          customer: res.data[0].first_name + ' ' + res.data[0].last_name
         });
       })
       .catch(err => this.setState({ error: err.message }));
     } else if (this.state.email) {
       API.findCustomerByEmail(this.state.email)
       .then(res => {
-        if (res.data.status === "error") {
+        if (res.data.status === 'error') {
           throw new Error(res.data.message);
         }
         this.setState({
           results: res.data,
-          error: "",
-          customers: "",
+          error: '',
+          customers: '',
           items: res.data[0].items,
           customerID: res.data[0]._id,
-          customer: res.data[0].first_name + " " + res.data[0].last_name
+          customer: res.data[0].first_name + ' ' + res.data[0].last_name
         });
       })
       .catch(err => this.setState({ error: err.message }));
@@ -118,7 +118,7 @@ class Return extends Component {
     API.updateItem(
       id,
       {
-        status: "In Maintenance",
+        status: 'In Maintenance',
         condition: condition
       }
     ).then(res => this.loadItems())
@@ -129,7 +129,7 @@ class Return extends Component {
     API.updateItem(
       id,
       {
-        status: "Available",
+        status: 'Available',
         condition: condition
       }
     ).then(res => this.loadItems())
@@ -149,13 +149,13 @@ class Return extends Component {
   loadItems = () => {
     API.findCustomerByLastName(this.state.last_name)
       .then(res => {
-        if (res.data.status === "error") {
+        if (res.data.status === 'error') {
           throw new Error(res.data.message);
         }
         this.setState({
           results: res.data,
-          error: "",
-          customers: "",
+          error: '',
+          customers: '',
           items: res.data[0].items,
           customerID: res.data[0]._id
         });
@@ -166,14 +166,14 @@ class Return extends Component {
 
   render() {
     return (
-      <div className="returnContainer">
+      <div className='returnContainer'>
         <Container>
           <Row>
-            <Col size="md-12">
+            <Col size='md-12'>
               <h2>Returning Rental Items?</h2>
               <Container>
                 <Row>
-                  <Col size="md-12">
+                  <Col size='md-12'>
                     <ReturnForm
                       handleFormSubmit={this.handleFormSubmit}
                       handleInputChange={this.handleInputChange}
@@ -184,7 +184,7 @@ class Return extends Component {
             </Col>
           </Row>
           <Row>
-            <Col size="md-12 sm-12">
+            <Col size='md-12 sm-12'>
               {this.state.items.length ? (
                 <ReturnResults>
                   <h3>Items Rented Out By {this.state.customer}</h3>
@@ -199,20 +199,20 @@ class Return extends Component {
                         <p>
                           Serial Number: {item.serial_number}
                         </p>
-                        <label htmlFor="item-condition">Update Item Condition:</label>
+                        <label htmlFor='item-condition'>Update Item Condition:</label>
                         <SelectCondition
-                          name={"itemCondition" + index}
+                          name={'itemCondition' + index}
                           value={this.state.itemCondition[index]}
                           handleChange={this.handleInputChange}
                         />
-                        <label htmlFor="item-comment">Add Comment About Item's Condition:</label>
+                        <label htmlFor='item-comment'>Add Comment About Item's Condition:</label>
                         <TextArea
                           value={this.state.itemComment[index]}
                           onChange={this.handleInputChange}
-                          name={"itemComment" + index}
+                          name={'itemComment' + index}
                         />
-                        <DeleteBtn onClick={() => { this.deleteItemFromCustomer(item._id); this.putInMaintenance(item._id, this.state["itemCondition" + index]); this.addComment(item._id, this.state["itemComment" + index]); }} />
-                        <ReturnBtn onClick={() => { this.deleteItemFromCustomer(item._id); this.makeAvailable(item._id, this.state["itemCondition" + index]); this.addComment(item._id, this.state["itemComment" + index]); }} />
+                        <DeleteBtn onClick={() => { this.deleteItemFromCustomer(item._id); this.putInMaintenance(item._id, this.state['itemCondition' + index]); this.addComment(item._id, this.state['itemComment' + index]); }} />
+                        <ReturnBtn onClick={() => { this.deleteItemFromCustomer(item._id); this.makeAvailable(item._id, this.state['itemCondition' + index]); this.addComment(item._id, this.state['itemComment' + index]); }} />
                       </ReturnResultsItem>
                     );
                   })}
